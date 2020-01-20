fa2e39a6713c971ca23731797546a8be027d0653
Giulio <giulio.pace93@gmail.com>
2020-01-20 18:07:53 +0100

    RNN done

--- RNN.py
+++ RNN.py
@@ -1,8 +1,12 @@
 import numpy as np
 import os 
-#from keras.models import Sequential
-#from keras.layers import SimpleRNN
+import logging
+import time
 
+from keras.models import Sequential, load_model
+from keras.layers import SimpleRNN
+from keras.optimizers import Adam
+from tqdm import tqdm
 
 def load_one(path):	
 	with open(path) as f:
@@ -22,8 +26,6 @@ def load_one(path):
 
 def determine_winner(player1, player2): 
 	#returns 0 if player1 wins, 1 if player2 wins, 0.5 if draw
-	
-
 	player1 = player1[-1]
 	player2 = player2[-1]
 
@@ -55,19 +57,37 @@ def datagen():
 		
 		winner = determine_winner(player1, player2)
 
-		yield (player1, np.array([1-winner])), (player2, np.array([winner]))
+		yield (player1.reshape(1, -1, player1.shape[1]), np.array([1-winner])), (player2.reshape(1, -1, player2.shape[1]), np.array([winner]))
 
 
+def define_model():
+	model = Sequential()
+	model.add(SimpleRNN(units=1, activation='sigmoid', input_shape=(None, 14)))
+	optim = Adam()
+	model.compile(loss="binary_crossentropy", optimizer=optim)
+	#model.summary()
+	return model
 
 
-data = datagen()
+if __name__ == "__main__":
+	start_time = time.time()
 
-print("\n\n", next(data))
+	logging.disable(logging.WARNING)
+	os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
+	epochs = 25
+	data = datagen()
+	
+	#model = define_model()
 
-#preprocess data
+	model = load_model("models/AlphaPace.h5")
+	print("model loaded")
 
-#model = Sequential()
-#model.add(SimpleRNN(units=1, activation='sigmoid'))
-
-
+	for (x1,y1),(x2,y2) in tqdm(data):
+		model.fit(x1, y1, epochs=epochs, verbose=0)
+		model.fit(x2, y2, epochs=epochs, verbose=0)
+	
+	model.save("models/AlphaPace.h5")
+	print(round((time.time() - start_time),2), "s")
+	
+	print("allgood")
